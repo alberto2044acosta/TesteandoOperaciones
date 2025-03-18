e4905e7eb3f01be32406348be33bd05b188faa4b
Alberto Acosta <albertoacosta1007@gmail.com>
2025-03-18 11:03:50 -0400

    haciendo un comentario general de los 4 tests

--- src/main/test/CalculadoraTest.java
+++ src/main/test/CalculadoraTest.java
@@ -1,27 +1,25 @@
+//Se realizan tests de suma, resta, multiplicacion y division
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class CalculadoraTest {
 
-    //Este es un test que suma dos numeros
     @Test
     void testSuma() {
         assertEquals(5, Calculadora.sumar(3, 2));
     }
 
-    //Este es un test que resta dos numeros
     @Test
     void testResta() {
         assertEquals(1, Calculadora.restar(3, 2));
     }
 
-    //Este es un test que multiplica dos numeros
     @Test
     void testMultiplicacion() {
         assertEquals(6, Calculadora.multiplicar(3, 2));
     }
-
-    //Este es un test que divide dos numeros
+    
     @Test
     void testDivision() {
         assertEquals(2, Calculadora.dividir(8, 4));
